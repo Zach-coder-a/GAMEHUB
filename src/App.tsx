d2ab8c3652b9a799da849a04324c09b26564b25f
Isaac Mayodi <mayodiisaac6@gmail.com>
2023-06-27 13:33:07 +0300

    Build ColorMode switch

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,9 @@ const App = () => {
           <Navbar />
         </GridItem>
         <Show above="lg">
-          <GridItem bg="gold" area={"aside"}>
-            Aside
-          </GridItem>
+          <GridItem area={"aside"}>Aside</GridItem>
         </Show>
-        <GridItem bg="dodgerBlue" area={"main"}>
-          Main
-        </GridItem>
+        <GridItem area={"main"}>Main</GridItem>
       </Grid>
     </div>
   );
